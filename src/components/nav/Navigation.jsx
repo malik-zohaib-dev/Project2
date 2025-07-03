37e83e429946b6be9b1e23aea39c118c33325f0e
Malik Zohaib Altaf <zohaibmalik1777@gmail.com>
2025-07-04 04:42:05 +0500

    image

--- src/components/nav/Navigation.jsx
+++ src/components/nav/Navigation.jsx
@@ -4,7 +4,7 @@ const Navigation = () => {
   return (
     <nav className="container">
         <div>
-            <img src="/images/logo.png" alt="logo" />
+            <img src="images/logo.png" alt="logo" />
         </div>
 
         <ul>
